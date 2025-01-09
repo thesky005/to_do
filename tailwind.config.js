ddce2f74813e30aa51c765a150971feef6d87849
thesky005 <skyelgire005@gmail.com>
2025-01-09 13:28:26 +0530

    tailwind addded

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // This will include your React/TSX files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
